e1258fbf6b4bab62a05a04bc55610905ff8ae9c8
Ronald Beltrán <71865035+rbeltran076@users.noreply.github.com>
2021-06-27 01:19:38 -0500

    Test para ver si se puede visualizar el código.

---
+++ inicio.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+
+Class Ventana 
+{
+	static void Main()
+	{
+		int someInteger = 3;
+		Windows.Forms.MessageBox.Show("Hello World");
+		Windows.Forms.MessageBox.Show(someInteger);
+		
+		
+	}
+}
